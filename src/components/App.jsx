e047f759fe3b01dadc971bd3d97fe304ce869eef
biswa418 <biswajeetsutar418@gmail.com>
2023-04-21 04:48:50 +0530

    initial setup - vite, react, tailwind, fonts

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='border-2 border-sky-500'>Hello world</div>
+    </>
+  )
+}
+
+export default App
