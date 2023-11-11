a677ace9be304fe1608b7628c0e1a0159ab1e659
Biswajeet Sutar <biswajeetsutar418@gmail.com>
2023-11-12 01:58:51 +0530

    with analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { App } from './components'
-import './styles/index.css'
-import './styles/page404.css'
-import { AuthProvider, PostProvider } from './providers'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./components";
+import "./styles/index.css";
+import "./styles/page404.css";
+import { AuthProvider, PostProvider } from "./providers";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <AuthProvider>
       <PostProvider>
         <App />
       </PostProvider>
     </AuthProvider>
-  // </React.StrictMode>
-)
+  </React.StrictMode>
+);
